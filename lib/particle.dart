4f9da264c30f69486fa44fec5d03494a63ba3efd
unknown <huutam.ptnk@gmail.com>
2018-11-22 20:25:11 +0200

    added particle.dart

---
+++ lib/particle.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Particle extends CustomPainter {
+  @override
+  void paint(Canvas canvas, Size size) {}
+
+  @override
+  bool shouldRepaint(CustomPainter oldDelegate) {
+    return true;
+  }
+}
