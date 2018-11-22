98b45c2cb11fc1000c160b9839f1733797041aa7
unknown <huutam.ptnk@gmail.com>
2018-11-22 20:41:02 +0200

    added constructor to particle.dart

--- lib/particle.dart
+++ lib/particle.dart
@@ -1,6 +1,23 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class Particle extends CustomPainter {
+  final Color color;
+  final double xCoor;
+  final double yCoor;
+  final double opacity;
+  final double size;
+
+  Random random = new Random();
+
+  Particle({
+    this.color,
+    this.xCoor,
+    this.yCoor,
+    this.opacity,
+    this.size,
+  });
+
   @override
   void paint(Canvas canvas, Size size) {}
 
