eabc82019ae458e17b683176ff4ffdf124879b28
unknown <huutam.ptnk@gmail.com>
2018-11-23 19:21:37 +0200

    add animation to particles.dart

--- lib/particles.dart
+++ lib/particles.dart
@@ -3,12 +3,49 @@ import 'package:particle/particle.dart';
 import 'package:flutter/animation.dart';
 
 class Particles extends StatefulWidget {
-  Particles({Key key, this.title}) : super(key: key);
+  Particles({Key key}) : super(key: key);
   @override
   _Particles_State createState() => new _Particles_State();
 }
 
-class _Particles_State extends State<Particles> with TickerProviderStateMixin {
+class _Particles_State extends State<Particles>
+    with SingleTickerProviderStateMixin {
+  Animation animation;
+  AnimationController animationController;
+  Particle a = new Particle(
+    color: Colors.black,
+    xCoor: 200.0,
+    yCoor: 300.0,
+    opacity: 1.0,
+    size: 3.0,
+  );
+
   @override
-  Widget build(BuildContext context) {}
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      duration: Duration(milliseconds: 1000),
+      vsync: this,
+    );
+
+    animationController.addListener(() {
+      setState(() {
+        a.move();
+      });
+    });
+    animationController.forward();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new CustomPaint(
+      foregroundPainter: a,
+    );
+  }
 }
