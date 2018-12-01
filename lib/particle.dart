983024240274388441601748dbb4c40a20e883d6
unknown <huutam.ptnk@gmail.com>
2018-12-01 16:35:23 +0200

    finish isNear method for a Particle

--- lib/particle.dart
+++ lib/particle.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'dart:math';
 
+const double CONNECT_DISTANCE = 5.0;
+
 class Particle {
   final Color color;
   double xCoor;
@@ -21,7 +23,19 @@ class Particle {
     this.size,
   });
 
-  void isNear(Particle anotherParticle) {}
+  bool isNear(Particle anotherParticle) {
+    // Calculate the distance between two particles
+    double distance = (this.xCoor - anotherParticle.xCoor) *
+            (this.xCoor - anotherParticle.xCoor) +
+        (this.yCoor - anotherParticle.yCoor) *
+            (this.yCoor - anotherParticle.yCoor);
+
+    if (distance <= CONNECT_DISTANCE) {
+      return true;
+    }
+    return false;
+  }
+
   void getRandomDirection() {
     xDirection = random.nextDouble();
     yDirection = random.nextDouble();
