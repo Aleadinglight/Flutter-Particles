11f81ef40f12b7631f135b42026291aa81f7d82f
unknown <huutam.ptnk@gmail.com>
2018-12-03 18:54:08 +0200

    delete unnecessary

--- lib/drawPoints.dart
+++ lib/drawPoints.dart
@@ -13,24 +13,15 @@ class DisplayPoints extends CustomPainter {
   void paint(Canvas canvas, Size size) {
     Paint line = new Paint();
     line.strokeCap = StrokeCap.round;
+    line.color = pointsList.elementAt(0).color;
     // Draw all the particles
     for (var point in pointsList) {
-      line.color = point.color;
       line.strokeWidth = point.size;
       Offset center = new Offset(point.xCoor, point.yCoor);
       line.style = PaintingStyle.fill;
       canvas.drawCircle(center, point.size, line);
     }
-    //
-    // Offset firstParticle = new Offset(
-    //     pointsList.elementAt(1).xCoor, pointsList.elementAt(1).yCoor);
-    // Offset secondParticle = new Offset(
-    //     pointsList.elementAt(2).xCoor, pointsList.elementAt(2).yCoor);
-    // line.color = Colors.white;
-    // line.strokeWidth = 1.0;
-    // print("drawline");
-    // canvas.drawLine(firstParticle, secondParticle, line);
-    // canvas.drawLine(secondParticle, firstParticle, line);
+
     // Draw the connect line
     for (int i = 0; i < this.pointsList.length; i++) {
       for (int j = i + 1; j < this.pointsList.length; j++) {
--- lib/particles.dart
+++ lib/particles.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/animation.dart';
 import 'package:particle/particle.dart';
 import 'package:particle/drawPoints.dart';
-import 'package:particle/drawLine.dart';
 import 'dart:math';
 
 class Particles extends StatefulWidget {
